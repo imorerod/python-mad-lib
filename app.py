d16e322e25b7379051ff970d06b69cb76cf779af
Isaac <imorerod@hotmail.com>
2019-08-08 09:30:39 -0500

    created three variables for user to input into

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 
-print('Roses are red')
-print('Violets are blue')
-print('I love you')
+color = input('Enter a color: ')
+plural_noun = input('Enter a plural noun: ')
+celebrity = input('Enter the name of a celebrity: ')
+
+print('Roses are {color}')
+print('{plural noun} are blue')
+print('I love {celebrity}')
 
