f09a9d9187b2b8a61482ec44279b9e20f093f043
Isaac <imorerod@hotmail.com>
2019-08-08 09:33:38 -0500

    game works as it should

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ color = input('Enter a color: ')
 plural_noun = input('Enter a plural noun: ')
 celebrity = input('Enter the name of a celebrity: ')
 
-print('Roses are {color}')
-print('{plural noun} are blue')
-print('I love {celebrity}')
+print('Roses are ' + color)
+print(plural_noun + ' are blue')
+print('I love ' + celebrity)
 
